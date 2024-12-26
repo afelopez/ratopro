86b21109d35132f7b8a429bdb8141c6b16de9640
Andrés López <dev.andres.lopez@gmail.com>
2024-12-26 08:43:14 -0500

    [Add] My first component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Todo List</h1>
+      <TodoItem />
+      <TodoCounter completed={2} totalx={5} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +19,24 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende para construir un mejor mundo, no para ganar más dinero.
         </a>
       </header>
     </div>
   );
 }
 
+function TodoItem() {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span>Texto de ejemplo</span>
+      <button>Eliminar</button>
+    </li>
+  );
+}
+
+function TodoCounter(props) {
+  return <h2>Has completado {props.completed} de {props.totalx} TODOs</h2>;
+}
 export default App;
